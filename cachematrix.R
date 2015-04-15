3b81808797cce5b3bc7272afb3b647bfdb63e75b
Yaron Harel <yarharel@gmail.com>
2015-04-15 23:28:00 +0300

    I think i'm done here

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## creates a list of functions of getters and setters to the matrix value (-1) value
 makeCacheMatrix <- function(x = matrix()) {
-
+    cS <- NULL 
+    set <- function(y) { 
+        x <<- y
+        cS <<- NULL
+    }
+    get <- function() x 
+    setS <- function(cacheS) cS <<- cacheS  
+    getS <- function() cS 
+    list(set = set, get = get,
+         setS = setS,
+         getS = getS)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## function returning the x(-1) from cache if it exists, 
+##  otherwise calculating it and storing it in cache
+cacheSolve <- function(x , ...) {
+    cS <- x$getS() 
+    if(!is.null(cS)) { 
+        message("getting cached data")
+        return(cS)
+    }
+    data <- x$get() 
+    cS <- solve(data, ...) 
+    x$setS(cS)
+    cS
 }
